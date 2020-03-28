6ddec7cbc619d274227292ea1b57869ceaa5b3ad
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-03-28 20:06:04 +0530

    added is odd function
    it uses is_even function and returns true when the number
    is odd or else false

--- basic_functions.c
+++ basic_functions.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 unsigned char is_even(int num);
+unsigned char is_odd(int num);
+
+unsigned char is_odd(int num){
+  return !is_even(num);
+}
 
 unsigned char is_even(int num){
   return num % 2 == 0;
@@ -8,6 +13,7 @@ unsigned char is_even(int num){
 
 int main(void)
 {
-  int num = 5;
+  int num = 2;
   printf("The number is %s\n",is_even(num)?"even":"not even");
+  printf("The number is %s\n", is_odd(num)? "odd" : "not odd");
 }

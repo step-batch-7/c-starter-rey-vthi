18b8f4fe91fe151a9bf977644f37e9297489c6b9
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-03-28 21:00:34 +0530

    added gcd function which find gcd of 2 numbers

--- basic_functions.c
+++ basic_functions.c
@@ -4,6 +4,12 @@ unsigned char is_even(int num1);
 unsigned char is_odd(int num1);
 int square(int num1);
 int cube(int num1);
+int gcd(int num1,int num2);
+
+int gcd(int num1, int num2)
+{
+  return num2!=0?gcd(num2,num1%num2):num1;
+}
 
 int cube(int num1){
   return square(num1)*num1;
@@ -24,9 +30,10 @@ unsigned char is_even(int num1){
 int main(void)
 {
   int num1 = 2;
+  int num2 = 3;
   printf("%d is %s\n",num1, is_even(num1)?"even":"not even");
   printf("%d is %s\n", num1, is_odd(num1)? "odd" : "not odd");
   printf("The square of %d is %d\n",num1,square(num1));
   printf("The cube of %d is %d\n",num1,cube(num1));
-
+  printf("Gcd of %d and %d is %d\n",num1,num2,gcd(num1,num2));
 }
